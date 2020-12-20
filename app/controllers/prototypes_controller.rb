a97cab24c05f23735d11c1ac9dcc890bc04adbbc
t-sasagawa <74903855+t-sasagawa@users.noreply.github.com>
2020-12-20 16:21:24 +0900

    コメントの修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -22,7 +22,7 @@ class PrototypesController < ApplicationController
   def show
     @prototype = Prototype.find(params[:id])
     @comment = Comment.new
-    @comments = @prototype.comment
+    @comments = @prototype.comments
   end
 
   def edit
